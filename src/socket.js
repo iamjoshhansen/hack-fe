ff4e1f97c852d28a83e1522fb4a258872cbec1ca
Joshua Hansen <hansenlj@us.ibm.com>
2017-11-22 15:35:47 -0700

    :crystal_ball: Prepping for dynamic socket/http communication.

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,13 @@
 import io from 'socket.io-client';
 
-const socket = io('http://192.168.0.55:2031/');
+const ip = '192.168.0.55';
+const port = 2031;
+const socket = io(`http://${ip}:${port}`);
+
+// fetch('http://iamjoshhansen.com/pi-ip/')
+//   .then(response => response.json())
+//   .then((json) => {
+//     socket.io.opts.hostname = `http://${json.ip}:${json.port}`;
+//  });
 
 export default socket;
